745c777c73039753ef6b3a20643f246b823c67a2
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-11-10 00:22:03 -0300

    Add files via upload
    Em um banco, para se cadastrar uma conta bancária, é necessário informar o número da conta, o nome do titular da conta, e o valor de depósito inicial que o titular depositou ao abrir a conta. Este valor de depósito inicial, entretanto, é opcional, ou seja: se o titular não tiver dinheiro a depositar no momento de abrir sua conta, o depósito inicial não será feito e o saldo inicial da conta será, naturalmente, zero.●Importante: uma vez que uma conta bancária foi aberta, o número da conta nunca poderá ser alterado, contudo o nome do titular pode ser alterado (ex.: pois uma pessoa pode mudar de nome por ocasião de casamento).●Por fim, o saldo da conta não pode ser alterado livremente e será preciso haver um mecanismo para proteger isso. O saldo só aumenta por meio de depósitos, e só diminuirá por meio de saques. Para cada saque realizado, o banco cobra uma taxa de R$ 5.00. Nota: a conta pode ficar com saldo negativo se o saldo não for suficiente para realizar o saque ou pagar a taxa.

---
+++ ContaBancaria.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace POO_TarefaBanco
+{
+    class ContaBancaria
+    {
+
+        /* Importante: uma vez que uma conta bancária foi aberta, 
+         * o número da conta nunca poderá ser alterado, 
+         * contudo o nome do titular pode ser alterado (ex.: pois uma pessoa pode mudar de nome por ocasião de casamento).
+         */
+        public int NumConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numConta, string nome)
+        {
+            NumConta = numConta;
+            Nome = nome;
+        }
+
+        public ContaBancaria(int numConta, string nome, double saldo) : this(numConta, nome)
+        {
+            Saldo = saldo;
+        }
+
+        /* Por fim, o saldo da conta não pode ser alterado livremente e 
+         * será preciso haver um mecanismo para proteger isso. 
+         * O saldo só aumenta por meio de depósitos, 
+         * e só diminuirá por meio de saques. 
+         */
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        /*Para cada saque realizado, 
+         * o banco cobra uma taxa de R$ 5.00. 
+         * Nota: a conta pode ficar com saldo negativo 
+         * se o saldo não for suficiente para realizar o saque ou pagar a taxa.
+         */
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        /* Realizar um depósito um saque, 
+         * sempre mostrando os dados da conta após cada operação.
+         */
+        public override string ToString()
+        {
+            return "Conta "
+                + NumConta
+                + ", Titular: "
+                + Nome
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO_TarefaBanco
+/*Em um banco, para se cadastrar uma conta bancária, é necessário informar:
+ * o número da conta, 
+ * o nome do titular da conta, e 
+ * o valor de depósito inicial que o titular depositou ao abrir a conta.
+ */
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            
+            Console.Write("Informe o número da conta: ");
+            int numConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o nome do titular da conta: ");
+            string nome = Console.ReadLine();
+
+            /* Um valor de depósito inicial,  é opcional, ou seja: se o titular não tiver dinheiro a depositar no momento de abrir sua conta, 
+             * o depósito inicial não será feito e 
+             * o saldo inicial da conta será, naturalmente, zero.
+             */
+            Console.Write("Haverá um depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Informe o valor de depósito inicial: ");
+                double depInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                conta = new ContaBancaria(numConta, nome, depInicial);
+            }
+
+            else
+            {
+                conta = new ContaBancaria(numConta, nome);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Informações da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Informe um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            conta.Deposito(quantia);
+            Console.WriteLine("Informações da conta atualizadas:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Informe o valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+
+            Console.WriteLine("Informações da conta atualizadas:");
+            Console.WriteLine(conta);
+
+        }
+    }
+}
